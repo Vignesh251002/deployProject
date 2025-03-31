d074a61052e65651466c4743666c90c560c9fd86
Vignesh251002 <vignesh.i@meyicloud.com>
2025-03-31 07:42:24 +0000

    Initial commit

---
+++ shellScript/deploy/deploy.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+Stage="dev"
+Project="sam-app2"
+Region="us-east-1"
+DynamoDBTableName="MyTable"
+S3Bucket="sam-bucket-25102002" 
+TemplateFile="../../template/templateShell.yaml"
+LambdaFunctionName="LambdaFunctionName"
+StackName="${Project}-stack"
+
+
+if ! aws s3 ls "s3://$S3Bucket" 
+then
+    echo "S3 bucket $S3Bucket does not exist. Creating..."
+    aws s3 mb "s3://$S3Bucket" --region $Region
+else
+    echo "S3 bucket $S3Bucket already exists."
+fi
+
+echo "Storing the template to S3."
+
+sam package \
+    --template-file $TemplateFile \
+    --s3-bucket $S3Bucket \
+    --output-template-file packagedDeploy-template.yaml \
+    --region $Region
+
+
+echo "Deploying application"
+
+sam deploy \
+    --template-file packagedDeploy-template.yaml \
+    --stack-name $StackName \
+    --s3-bucket $S3Bucket \
+    --region $Region \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM \
+    --parameter-overrides \
+        Stage=${Stage} \
+        DynamoDBTableName=${DynamoDBTableName} \
+        LambdaFunctionName=${LambdaFunctionName}
+
+echo "deployment complete!"
---
+++ src/index.mjs
@@ -0,0 +1,29 @@
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+
+const dynamoDB = new DynamoDBClient({ region: "us-east-1" });
+
+export const handler = async (event) => {
+    try {
+        const { user_id, name, age } = JSON.parse(event.body);
+
+        const params = {
+            TableName: "MyTable",
+            Item: {
+                user_id: { S: String(user_id) }, 
+                name: { S: name },
+                age: { N: String(age) }
+            }
+        };
+        await dynamoDB.send(new PutItemCommand(params));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Item inserted successfully" }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+};
